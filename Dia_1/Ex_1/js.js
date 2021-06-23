35509159b1236fdba3aea3b4ecb944767c87fdd3
Matheus <mateeus.soarees@outlook.com>
2021-06-23 18:20:00 -0300

    Ex 1

---
+++ Dia_1/Ex_1/js.js
@@ -0,0 +1,24 @@
+a = 2;
+b = 2;
+
+selector = "Adição"
+
+switch (selector) {
+    case "Adição":
+        console.log(a+b);
+        break;
+    case "Adição":
+        console.log(a+b);
+        break;
+    case "Adição":
+        console.log(a+b);
+        break;
+    case "Adição":
+        console.log(a+b);
+        break;
+    case "Adição":
+        console.log(a+b);
+        break;  
+    default:
+        break;
+}
