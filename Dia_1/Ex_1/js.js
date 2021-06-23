d5084d9712c43e35f923b000b54ac971834d0681
Matheus <mateeus.soarees@outlook.com>
2021-06-23 18:41:13 -0300

    Ex 3 e ajustes nos casos do ex 1

--- Dia_1/Ex_1/js.js
+++ Dia_1/Ex_1/js.js
@@ -7,18 +7,18 @@ switch (selector) {
     case "Adição":
         console.log(a+b);
         break;
-    case "Adição":
-        console.log(a+b);
+    case "Subtração":
+        console.log(a-b);
         break;
-    case "Adição":
-        console.log(a+b);
+    case "Divisão":
+        console.log(a/b);
         break;
-    case "Adição":
-        console.log(a+b);
+    case "Multiplicação":
+        console.log(a*b);
         break;
-    case "Adição":
-        console.log(a+b);
-        break;  
+    case "Módulo":
+        console.log(a%b);
+        break;    
     default:
         break;
 }
---
+++ Dia_1/Ex_3/js.js
@@ -0,0 +1,15 @@
+ var a = 1;
+ var b = 5;
+ var c = 3;
+
+if ( a > b && a > c) {
+    console.log (a);
+} else {}
+ 
+if ( b > a && b > c ) {
+    console.log(b);
+} else{}
+
+if ( c > a && c > b ) {
+    console.log(c);
+}
