39c9cbd0dfba69f3d85f89dc5794152e8a93a9e2
Matheus <mateeus.soarees@outlook.com>
2021-06-24 18:30:28 -0300

    Ex 4

---
+++ Dia_2/Ex_4/js.js
@@ -0,0 +1,11 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+var soma = 0;
+for (let index = 0; index < numbers.length; index++) {
+    soma += numbers[index];
+    var media = soma/numbers.length;
+}
+if (media > 20) {
+    console.log("valor maior que 20");
+} else if (media <= 20) {
+    console.log("valor menor ou igual a 20");
+}
