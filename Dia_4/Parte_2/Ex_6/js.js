1aa74eb5990201a20352c355c8ddc8e7ba3cb1eb
Matheus <mateeus.soarees@outlook.com>
2021-06-29 12:56:53 -0300

    Ex_6 - parte 2

---
+++ Dia_4/Parte_2/Ex_6/js.js
@@ -0,0 +1,8 @@
+    function somaToria(n) {
+        total=0;
+        for (let index = 1; index <= n; index++) {
+            total = total+index;
+            }
+        return total;
+    }
+console.log(somaToria(5));
